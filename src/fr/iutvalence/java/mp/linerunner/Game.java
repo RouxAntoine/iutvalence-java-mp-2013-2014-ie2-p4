5e86776016d01211b25287610704d80ef132adf4
sebastienjean <baz.jean@gmail.com>
2013-09-19 21:42:10 +0200

    Added some TODOs
    These TODOs must be processed before doing anything else

--- src/fr/iutvalence/java/mp/linerunner/Game.java
+++ src/fr/iutvalence/java/mp/linerunner/Game.java
@@ -1,5 +1,7 @@
 package fr.iutvalence.java.mp.linerunner;
 
+// TODO (fix) Comment syntax does not follow Javadoc guidelines
+// TODO (fix) Comment must be completed
 /*
  *the class who contain character and the 
  */
--- src/fr/iutvalence/java/mp/linerunner/linerunner.java
+++ src/fr/iutvalence/java/mp/linerunner/linerunner.java
@@ -1,7 +1,10 @@
 package fr.iutvalence.java.mp.linerunner;
 
+// TODO (fix) Class name does not follow naming guidelines
 public class linerunner
 {
+    // TODO (fix) Misplaced comment (must be before class declaration)
+    
     /*
      * set of level in the game
      * this class contain the main menu:
