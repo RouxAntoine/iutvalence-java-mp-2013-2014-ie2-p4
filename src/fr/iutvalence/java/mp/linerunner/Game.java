21acd8ea9029638e75c3419c7d2430263746fd0c
rouxan <antoine.roux@iut-valence.fr>
2013-09-26 19:49:10 +0200

    update and add comment of the variables

--- src/fr/iutvalence/java/mp/linerunner/Game.java
+++ src/fr/iutvalence/java/mp/linerunner/Game.java
@@ -3,7 +3,7 @@ package fr.iutvalence.java.mp.linerunner;
 // TODO (fixed) Comment syntax does not follow Javadoc guidelines
 // TODO (fixed) Comment must be completed
 /**
- *the class who contain character and the 
+ *the class who contain character and the composante of the game
  */
 
 public class Game
--- src/fr/iutvalence/java/mp/linerunner/Grid.java
+++ src/fr/iutvalence/java/mp/linerunner/Grid.java
@@ -10,16 +10,16 @@ public class Grid
     int ligne;
     int colonne;
     
-    private int character=3 ;
-    private int low_hurdle=0;
-    private int nothing=1;
-    private int hight_hurdle=2;
-    private int ground=4;
+    private int character=3 ;               //identification of the characters on the grid
+    private int low_hurdle=0;               //identification of the low_hurdle on the grid
+    private int nothing=1;                  //identification of void space on the grid
+    private int high_hurdle=2;             //identification of the high_hurdle on the grid
+    private int ground=4;                   //identification of the ground on the grid
         
     public Grid()
     {
     /**
-     * @param
+     * @param void
      */
         int[][]t= new int [10][5];        
     }
